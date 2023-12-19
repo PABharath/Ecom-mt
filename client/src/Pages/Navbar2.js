df7fd1ada2cdac8a21f636c47c9cd263641095ba
satyavani09 <145200987+satyavani09@users.noreply.github.com>
2023-12-19 19:28:22 +0530

    navbar

--- client/src/Pages/Navbar2.js
+++ client/src/Pages/Navbar2.js
@@ -44,27 +44,28 @@ const Navbar2 = ({ onSearch }) => {
 
   return (
     <div>
-    <Navbar  expand="lg" className={styles.navbara}>
+   <Navbar  expand="lg" className={styles.navbara}>
       <Container className={styles.navbarContainera}>
-        <Navbar.Brand as={NavLink} to="/" style={{color:'white'}} className='logovik' >E-Saree </Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/"  className='logovik' >
+        <h3>E-Saree</h3>
+         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="me-autovik">
 
-          <Nav.Link as={NavLink} to="/" style={{color:'white'}} className={styles.navLink}>
-              Home
+          <Nav.Link as={NavLink} to="/"  className={styles.navLink}>
+            Home
             </Nav.Link>
            
-            <Nav.Link as={NavLink} to="/SareesCategories2" style={{color:'white'}} className={styles.navLink}>
+            <Nav.Link as={NavLink} to="/SareesCategories2"  className={styles.navLink}>
               Saree
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/BlogPost" style={{color:'white'}} className={styles.navLink}>
+            <Nav.Link as={NavLink} to="/BlogPost"  className={styles.navLink}>
               Blog
             </Nav.Link>
-            <Nav.Link as={NavLink} to="/ContactUs" style={{color:'white'}} className={styles.navLink}>
+            <Nav.Link as={NavLink} to="/ContactUs"  className={styles.navLink}>
               Contact Us
             </Nav.Link>
-            <Nav.Link as={NavLink}  className={styles.navLink}>
             <div className='nav-searchbarvik'>
 
             <input
@@ -86,8 +87,6 @@ const Navbar2 = ({ onSearch }) => {
             
 
 
-            </Nav.Link>
-            <Nav.Link as={NavLink}  className={styles.navLink}>
 
 
                  <div className="navvik-right">
@@ -95,13 +94,13 @@ const Navbar2 = ({ onSearch }) => {
           <FontAwesomeIcon icon={faPlus} className="menu-icon" />
         </Link>
 
-        <Link to="/cart" style={{color:'white'}}>
+        <Link to="/cart" >
           <FontAwesomeIcon icon={faShoppingCart} />
           <span>{totalQuantity}</span>
         </Link>
 
         <div className="tooltip-container">
-          <FontAwesomeIcon icon={faBell} style={{color:'white'}} className="menu-icon" />
+          <FontAwesomeIcon icon={faBell}  className="menu-icon" />
           <div className="tooltip">No new notifications.. Stay tuned for more!!</div>
         </div>
 
@@ -115,7 +114,7 @@ const Navbar2 = ({ onSearch }) => {
           </Link>
         )}
 
-<Button  ref={target} style={{backgroundColor:'#8eab92',color:'white',border:'none'}}  onClick={() => setShow(!show)}>
+<Button  ref={target} style={{backgroundColor:'white',color:'white',border:'none'}}  onClick={() => setShow(!show)}>
            
            <FaUserCircle style={{fontSize:'1.2rem' , marginTop:'-1rem',marginLeft:'-0.8rem'}}/>
        
@@ -146,7 +145,7 @@ const Navbar2 = ({ onSearch }) => {
 
 
 
-            </Nav.Link>
+           
           </Nav>
         </Navbar.Collapse>
       </Container>
