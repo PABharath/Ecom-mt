3077ba62d89fd0c4bb9ea998a1901162b0ba718c
karishma5821 <145200862+karishma5821@users.noreply.github.com>
2023-12-20 15:25:45 +0530

    orders

--- client/src/Pages/Orders.js
+++ client/src/Pages/Orders.js
@@ -1,3 +1,135 @@
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+// import "./Orders.css";
+// import Navbar2 from "./Navbar2";
+
+// const Orders = () => {
+//   const [orders, setOrders] = useState([]);
+
+//   useEffect(() => {
+//     fetchOrders();
+//   }, []);
+
+//   const fetchOrders = async () => {
+//     try {
+//       const response = await axios.get("http://localhost:5555/api/orders");
+
+//       const data = response.data.orders;
+
+//       const ordersWithFormattedData = data.map((order) => {
+//         return {
+//           orderId: order.orderId,
+//           orderDate: new Date(order.orderDate).toLocaleDateString(),
+//           totalAmount: order.totalAmount,
+//           expectedDeliveryDate: order.expectedDeliveryDate,
+//           products: order.products.map((product) => {
+//             return {
+//               productName: product.productName,
+//               productImages: product.productImages,
+//               productId: product.productId, // Make sure productId is available
+//             };
+//           }),
+//         };
+//       });
+
+//       setOrders(ordersWithFormattedData);
+//     } catch (error) {
+//       console.error("Error fetching orders:", error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Navbar2 />
+//       <h1>Your Orders</h1>
+//       <div className="ecom-container">
+//         {orders.map((order) => (
+//           <div key={order.orderId} className="order-table-container">
+//             <table className="ecom-order-table">
+//               <thead>
+//                 <tr>
+//                   <th>ORDER PLACED</th>
+//                   <th>TOTAL</th>
+//                   <th>SHIP TO</th>
+//                   <th>Order ID</th>
+//                   <th>View Order Details</th>
+//                 </tr>
+//               </thead>
+//               <tbody>
+//                 <tr>
+//                   <td>{order.orderDate}</td>
+//                   <td>₹{order.totalAmount}</td>
+//                   <td>Address</td>
+//                   <td> {order.orderId}</td>
+//                   <td>
+//                     <Link
+//                       className="ecom-link"
+//                       to={`/invoice/${order.orderId}`}
+//                     >
+//                       View Invoice
+//                     </Link>
+//                   </td>
+//                 </tr>
+//               </tbody>
+//             </table>
+
+//             <div className="ecom-delivery-info-container">
+//               <div className="ecom-image-box">
+//                 {/* Assuming product image URL is available in the first product */}
+//                 <img
+//                   src={`http://127.0.0.1:5555/api/uploads/${
+//                     order.products[0]?.productImages?.[0] || "default-image.jpg"
+//                   }`}
+//                   alt="Product"
+//                   className="ecom-image"
+//                   onLoad={() => console.log("Image loaded successfully")}
+//                   onError={(e) => console.error("Error loading image:", e)}
+//                 />
+//               </div>
+//               <div className="ecom-delivery-details">
+//                 <p>
+//                   Delivery By{" "}
+//                   {new Date(order.expectedDeliveryDate).toLocaleDateString()}
+//                 </p>
+
+//                 {order.products && order.products.length > 0 ? (
+//                   order.products.map((product) => (
+//                     <p key={product.productName} className="ecom-product-name">
+//                       {product.productName}
+//                     </p>
+//                   ))
+//                 ) : (
+//                   <p>No products in this order</p>
+//                 )}
+//                 <div key={order._id} className="ecom-buttons">
+//                   <button className="ecom-view-items-btn">
+//                     <button className="ecom-view-items-btn">
+//                       <Link
+//                         to={`/products/${order.products[0]?.productId}`}
+//                         className="ecom-link-a"
+//                       >
+//                         View Your Item
+//                       </Link>
+//                     </button>
+//                   </button>
+
+//                   <button className="ecom-delivery-status-btn">
+//                     Delivery Status
+//                   </button>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Orders;
+
+
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -11,12 +143,15 @@ const Orders = () => {
     fetchOrders();
   }, []);
 
+
+
   const fetchOrders = async () => {
     try {
       const response = await axios.get("http://localhost:5555/api/orders");
-
+      console.log("API Response:", response);
+  
       const data = response.data.orders;
-
+  
       const ordersWithFormattedData = data.map((order) => {
         return {
           orderId: order.orderId,
@@ -27,17 +162,51 @@ const Orders = () => {
             return {
               productName: product.productName,
               productImages: product.productImages,
-              productId: product.productId, // Make sure productId is available
+              productId: product.productId,
             };
           }),
         };
       });
-
+  
+      console.log("Formatted Orders:", ordersWithFormattedData);
+  
       setOrders(ordersWithFormattedData);
     } catch (error) {
       console.error("Error fetching orders:", error);
     }
   };
+  
+
+  // const fetchOrders = async () => {
+  //   try {
+  //     const response = await axios.get("http://localhost:5555/api/orders");
+  //     console.log("API Response:", response);
+
+  //     const data = response.data.orders;
+
+  //     const ordersWithFormattedData = data.map((order) => {
+  //       return {
+  //         orderId: order.orderId,
+  //         orderDate: new Date(order.orderDate).toLocaleDateString(),
+  //         totalAmount: order.totalAmount,
+  //         expectedDeliveryDate: order.expectedDeliveryDate,
+  //         products: order.products.map((product) => {
+  //           return {
+  //             productName: product.productName,
+  //             productImages: product.productImages,
+  //             productId: product.productId, // Make sure productId is available
+  //           };
+  //         }),
+  //       };
+  //     });
+
+  //     console.log("Formatted Orders:", ordersWithFormattedData);
+
+  //     setOrders(ordersWithFormattedData);
+  //   } catch (error) {
+  //     console.error("Error fetching orders:", error);
+  //   }
+  // };
 
   return (
     <div>
@@ -63,12 +232,16 @@ const Orders = () => {
                   <td>Address</td>
                   <td> {order.orderId}</td>
                   <td>
-                    <Link
+                    {/* <Link
                       className="ecom-link"
                       to={`/invoice/${order.orderId}`}
                     >
                       View Invoice
-                    </Link>
+                    </Link> */}
+                    <Link className="ecom-link" to={`/invoice/${order.orderId}`}>
+                        View Invoice
+                          </Link>
+
                   </td>
                 </tr>
               </tbody>
@@ -128,3 +301,4 @@ const Orders = () => {
 };
 
 export default Orders;
+
