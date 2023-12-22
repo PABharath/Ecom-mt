da291e16b98c06ef18a4ea1b4ceab79683c78827
Nitinkaroshi <nitinkaroshi1@gmail.com>
2023-12-22 18:58:44 +0530

    KARISHMA

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 // controllers/userController.js
-const bcrypt = require('bcrypt');
-const User = require('../models/userModel');
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 const SALT_ROUNDS = 10;
 
@@ -12,15 +12,21 @@ exports.registerUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
     // Create a new user document
-    const newUser = new User({ username, email, contact, address, password: hashedPassword });
+    const newUser = new User({
+      username,
+      email,
+      contact,
+      address,
+      password: hashedPassword,
+    });
 
     // Save the user document to the database
     await newUser.save();
 
-    res.status(201).json({ message: 'Registration successful.' });
+    res.status(201).json({ message: "Registration successful." });
   } catch (error) {
-    console.error('Error inserting data:', error);
-    res.status(500).json({ error: 'Failed to register.' });
+    console.error("Error inserting data:", error);
+    res.status(500).json({ error: "Failed to register." });
   }
 };
 
@@ -32,8 +38,8 @@ exports.saveUserAddress = async (req, res) => {
   try {
     const customer = await User.findOne({ email: customerId }); // Use "email" to find the customer
     if (!customer) {
-      console.log('Customer not found for email:', customerId);
-      return res.status(404).json({ error: 'Customer not found.' });
+      console.log("Customer not found for email:", customerId);
+      return res.status(404).json({ error: "Customer not found." });
     }
 
     // Add the new address to the user's addresses
@@ -42,13 +48,13 @@ exports.saveUserAddress = async (req, res) => {
     // Save the updated customer document
     await customer.save();
 
-    console.log('Address added successfully:', addressData);
+    console.log("Address added successfully:", addressData);
 
     res.status(201).json({ addressData });
   } catch (error) {
-    console.error('Error saving address data:', error.message);
-    console.log('customerId:', customerId);
-    res.status(500).json({ error: 'Error saving address data.' });
+    console.error("Error saving address data:", error.message);
+    console.log("customerId:", customerId);
+    res.status(500).json({ error: "Error saving address data." });
   }
 };
 
@@ -59,24 +65,22 @@ exports.getUserProfile = async (req, res) => {
 
     // Fetch user details from the "users" collection and populate additional data
     const user = await User.findById(userId)
-    .populate('cart.product', 'productName sp quantity') 
-      .populate('wishlist', 'productName')
-      .populate('reviews', 'comment');
-      .select('-password') // Exclude password from the response
-      .populate('address'); // Populate user addresses
-
+      .populate("cart.product", "productName sp quantity")
+      .populate("wishlist", "productName")
+      .populate("reviews", "comment")
+      .select("-password") // Exclude password from the response
+      .populate("address"); // Populate user addresses
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     res.status(200).json({ user });
   } catch (error) {
-    console.error('Error fetching user profile:', error);
-    res.status(500).json({ error: 'Failed to fetch user profile' });
+    console.error("Error fetching user profile:", error);
+    res.status(500).json({ error: "Failed to fetch user profile" });
   }
 };
-
 
 // New function to add a product to the user's cart
 exports.addToCart = async (req, res) => {
@@ -86,8 +90,8 @@ exports.addToCart = async (req, res) => {
   try {
     const customer = await User.findOne({ email: customerId }); // Use "email" to find the customer
     if (!customer) {
-      console.log('Customer not found for email:', customerId);
-      return res.status(404).json({ error: 'Customer not found.' });
+      console.log("Customer not found for email:", customerId);
+      return res.status(404).json({ error: "Customer not found." });
     }
 
     // Clear the existing cart and add the new cart items
@@ -96,13 +100,13 @@ exports.addToCart = async (req, res) => {
     // Save the updated customer document
     await customer.save();
 
-    console.log('Product added to cart successfully:', cartItems);
+    console.log("Product added to cart successfully:", cartItems);
 
     res.status(201).json({ cartItems });
   } catch (error) {
-    console.error('Error saving product data:', error.message);
-    console.log('customerId:', customerId);
-    res.status(500).json({ error: 'Error saving product data.' });
+    console.error("Error saving product data:", error.message);
+    console.log("customerId:", customerId);
+    res.status(500).json({ error: "Error saving product data." });
   }
 };
 
@@ -114,8 +118,8 @@ exports.addToWishlist = async (req, res) => {
   try {
     const customer = await User.findOne({ email: customerId }); // Use "email" to find the customer
     if (!customer) {
-      console.log('Customer not found for email:', customerId);
-      return res.status(404).json({ error: 'Customer not found.' });
+      console.log("Customer not found for email:", customerId);
+      return res.status(404).json({ error: "Customer not found." });
     }
 
     // Add the new wishlist item
@@ -124,24 +128,24 @@ exports.addToWishlist = async (req, res) => {
     // Save the updated customer document
     await customer.save();
 
-    console.log('Product added to wishlist successfully:', wishlistItem);
+    console.log("Product added to wishlist successfully:", wishlistItem);
 
     res.status(201).json({ wishlistItem });
   } catch (error) {
-    console.error('Error saving wishlist item data:', error.message);
-    console.log('customerId:', customerId);
-    res.status(500).json({ error: 'Error saving wishlist item data.' });
+    console.error("Error saving wishlist item data:", error.message);
+    console.log("customerId:", customerId);
+    res.status(500).json({ error: "Error saving wishlist item data." });
   }
 };
 exports.addAddress = async (req, res) => {
   const customerId = req.params.id; // Correctly access customerId
   const appointmentData = req.body;
- console.log(req.params.id);
+  console.log(req.params.id);
   try {
     const customer = await User.findOne({ email: customerId }); // Use "_id" to find the customer
     if (!customer) {
-      console.log('Customer not found for _id:', customerId);
-      return res.status(404).json({ error: 'Customer not found.' });
+      console.log("Customer not found for _id:", customerId);
+      return res.status(404).json({ error: "Customer not found." });
     }
 
     // Add the new appointment to the customer's appointments array
@@ -150,13 +154,12 @@ exports.addAddress = async (req, res) => {
     // Save the updated customer document
     await customer.save();
 
-    console.log('Appointment data saved successfully:', appointmentData);
+    console.log("Appointment data saved successfully:", appointmentData);
 
     res.status(201).json(appointmentData);
   } catch (error) {
-    console.error('Error saving appointment data:', error.message);
-    console.log('customerId:', customerId);
-    res.status(500).json({ error: 'Error saving appointment data.' });
+    console.error("Error saving appointment data:", error.message);
+    console.log("customerId:", customerId);
+    res.status(500).json({ error: "Error saving appointment data." });
   }
 };
- 
