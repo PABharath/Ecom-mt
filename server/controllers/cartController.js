8f6952ec7a202e7dab5a9a1d95a55ff65dbd5274
Nitinkaroshi <nitinkaroshi1@gmail.com>
2023-12-23 14:36:31 +0530

    backend error fixed

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -52,44 +52,14 @@ exports.decrementCartItem = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    const cartItem = user.cart.find(item => item.product == productId);
-    if (!cartItem) {
-      return res.status(404).json({ message: 'Cart item not found' });
-    }
-
-    if (cartItem.quantity > 1) {
-      cartItem.quantity -= 1;
-      await user.save();
-      res.status(200).json({ message: 'CartItem decremented successfully', user: { cart: user.cart } });
-    } else {
-      // Remove the item from the cart if quantity becomes 0
-      user.cart = user.cart.filter(item => item.product != productId);
-      await user.save();
-      res.status(200).json({ message: 'CartItem removed successfully', user: { cart: user.cart } });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-};
-
-exports.decrementCartItem = async (req, res) => {
-  try {
-    const userId = req.user.id;
-    const { productId } = req.body;
-
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
     const updatedCart = user.cart.map((item) => {
       if (item.product == productId) {
         if (item.quantity > 1) {
+          // Reduce quantity by 1
           return { ...item, quantity: item.quantity - 1 };
         } else {
-          // Remove the item from the cart if quantity becomes 0
-          return null;
+          // Do nothing when quantity is 1 (or you can handle differently)
+          return item;
         }
       }
       return item;
@@ -99,6 +69,28 @@ exports.decrementCartItem = async (req, res) => {
     await user.save();
 
     res.status(200).json({ message: 'CartItem decremented successfully', user: { cart: updatedCart } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+exports.deleteCartItem = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { productId } = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Remove the item from the cart
+    const updatedCart = user.cart.filter(item => item.product != productId);
+    user.cart = updatedCart;
+    await user.save();
+
+    res.status(200).json({ message: 'CartItem deleted successfully', user: { cart: updatedCart } });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
