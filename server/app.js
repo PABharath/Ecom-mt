e91e570c42e15f28b75b0e590e765549eb36259f
Nitinkaroshi <nitinkaroshi1@gmail.com>
2023-12-27 20:19:37 +0530

    commit 2nd

--- server/app.js
+++ server/app.js
@@ -1,33 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const searchRoutes = require('./routes/searchRoutes');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
+const productRoutes = require('./routes/productRoutes'); 
 const addProductRoutes = require('./routes/addProductRoutes');
 const addressRoutes = require('./routes/addressRoutes');
 const forgotRoutes = require('./routes/forgotRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const emailRoutes = require('./routes/emailRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const reviewsRoutes = require('./routes/reviewsRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const loginRoutes = require('./routes/loginRoutes');
 
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB
+const MONGO_URI = 'mongodb+srv://lkasireddy915:1234@e-commerce.o3n1dk4.mongodb.net/Ecommerce?retryWrites=true&w=majority';
+// const dbName = 'Ecommerce-mvc';
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+  mongoose.connection.on('error', (error) => {
+    console.error('MongoDB connection error:', error);
+  });
+  
+
 // Serve uploaded images
 app.use('/api/uploads', express.static('uploads'));
 app.use('/api/search', searchRoutes);
-
 // Use the auth routes
 app.use('/auth', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', loginRoutes);
-
 // Use the product routes
 app.use('/api', productRoutes);
 
@@ -40,10 +57,10 @@ app.use('/api/address', addressRoutes);
 // Use the forgot password routes
 app.use('/api', forgotRoutes);
 
-// Use the Contact us
+// use the Contact us
 app.use('/contact', contactRoutes);
 
-// Use the mail
+// use the mail
 app.use('/login', emailRoutes);
 
 app.use('/api', reviewsRoutes);
@@ -58,10 +75,11 @@ const paymentRoutes = require('./routes/paymentRoutes');
 app.use('/api/payment', paymentRoutes);
 
 // Use order routes
-app.use('/api', orderRoutes);
+app.use("/api", orderRoutes);
 
 // Use the cart routes
 app.use('/api/profile', cartRoutes);
+
 
 const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => {
