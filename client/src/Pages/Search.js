b7f94feebbdfad41591499789a16df9171650d5c
Laxmikasi <136251521+Laxmikasi@users.noreply.github.com>
2023-12-30 18:59:40 +0530

    search changes

--- client/src/Pages/Search.js
+++ client/src/Pages/Search.js
@@ -36,9 +36,9 @@ export default function Search() {
 
     <form>
     <div className="Search-wrapper">
-      <button className="search-btn">
+      {/* <button className="search-btn">
         <BsSearch />
-      </button>
+      </button> */}
       <div className="form-group1">
         <input
           type="text"
